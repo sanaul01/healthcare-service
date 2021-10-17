857caeddebe0e6d8790d65d1d03c41395c36729d
sanaul01 <mdsanaulislam154@gmail.com>
2021-10-18 00:37:31 +0600

    route firecbase bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Smart Diagnostic Center</h1>
+      <h3>install firebase</h3>
     </div>
   );
 }
---
+++ src/Pages/Home/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDwdIxRA03avSMrYxVlPrt7nYlnhdiTMiw",
+    authDomain: "react-smart-diagnostic-center.firebaseapp.com",
+    projectId: "react-smart-diagnostic-center",
+    storageBucket: "react-smart-diagnostic-center.appspot.com",
+    messagingSenderId: "702718525624",
+    appId: "1:702718525624:web:ba6bf12ca3eaf7c042b44d"
+  };
+
+  export default firebaseConfig;
