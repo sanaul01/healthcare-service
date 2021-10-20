444caf31e0132218669482d166a490a4c810aff7
sanaul01 <mdsanaulislam154@gmail.com>
2021-10-20 23:29:19 +0600

    modify detail

--- src/Pages/Home/Home/OurService/OurService.js
+++ src/Pages/Home/Home/OurService/OurService.js
@@ -5,7 +5,7 @@ const OurService = () => {
     return (
         <div className="container">
             <div className="ourservice-container">
-            <h1>This is our services</h1>
+            <h1>This is our services sistem</h1>
             <h2>Smart Diagnostic Center</h2>
             <img src="https://media-eng.dhakatribune.com/uploads/2020/03/dhaka-tribune-1584731405845.jpg" alt="" />
             </div>
